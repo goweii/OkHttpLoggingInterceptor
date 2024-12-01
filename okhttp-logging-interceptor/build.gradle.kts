2e7a701c0e6a51118c5b9e954a56ed06a329196c
goweii <goweii@163.com>
2024-12-01 16:15:05 +0800

    Remove Android dependencies

--- okhttp-logging-interceptor/build.gradle.kts
+++ okhttp-logging-interceptor/build.gradle.kts
@@ -1,39 +1,10 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
-}
-
-android {
-    namespace = "per.goweii.okhttp3.logging"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 16
-
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+    alias(libs.plugins.jetbrains.kotlin.jvm)
 }
 
 dependencies {
     testImplementation(libs.junit)
 
-    compileOnly("com.squareup.okhttp3:okhttp:4.12.0")
-    testImplementation("com.squareup.okhttp3:okhttp:4.12.0")
+    compileOnly(libs.okhttp)
+    testImplementation(libs.okhttp)
 }
